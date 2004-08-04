0f919bc18c751fc7966281cf0ecf23ffa0f14b5b
heidineu <heidineu>
2004-08-04 09:06:20 +0000

    updated switch statement to retrieve platform info

--- plugin/platform.sh
+++ plugin/platform.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-HW=`uname -m`;
+HW=`uname -i`;
 
 if echo $HW | grep "i386" >/dev/null 
-    then echo "INTEL"
-elif echo $HW | grep "i486" >/dev/null
-    then echo "INTEL"
-elif echo $HW | grep "i586" >/dev/null 
-    then echo "INTEL"
-elif echo $HW | grep "i686" >/dev/null 
     then echo "INTEL"
 elif echo $HW | grep "s390" >/dev/null 
     then echo "S390"
 elif echo $HW | grep "ppc" >/dev/null 
     then echo "PPC"
 elif echo $HW | grep "x86_64" >/dev/null 
-    then echo "AMD64"
+    then echo "X86_64"
 elif echo $HW | grep "ia64" >/dev/null 
     then echo "IA64"
 else
